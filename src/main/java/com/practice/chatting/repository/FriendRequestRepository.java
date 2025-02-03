cb9d4787f336b9e109c423b2a4f76e18a59eaa39
kitoha <kitoha@daum.net>
2025-02-03 17:35:51 +0900

    [feature/userAuth] mysql 데이터베이스 설정 추가.

--- src/main/java/com/practice/chatting/repository/FriendRequestRepository.java
+++ src/main/java/com/practice/chatting/repository/FriendRequestRepository.java
@@ -8,11 +8,13 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
   List<FriendRequest> findByToUserAndStatus(User toUser, RequestStatus status);
 
   @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @Query("SELECT fr FROM FriendRequest fr WHERE fr.fromUser = :fromUser AND fr.toUser = :toUser")
   Optional<FriendRequest> findByFromUserAndToUserWithLock(User fromUser, User toUser);
 
 }
